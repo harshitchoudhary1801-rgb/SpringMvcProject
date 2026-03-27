e6d506eae59dcb803be223ebd00e41e155593aef
Harshit Kumar <harshitchoudhary1801@gmail.com>
2026-03-27 11:11:21 +0530

    added prints

---
+++ src/SwapNumber.java
@@ -0,0 +1,13 @@
+public class SwapNumber {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("Swapped Number:");
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
